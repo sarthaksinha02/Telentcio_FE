ebdccb09e674f08007c83513d5aa87e9d5bd7ea4
aman <tomaraman50@gmail.com>
2026-01-29 16:29:02 +0530

    feat: add Axios instance for API communication with token interception and ignore .env file

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: `${import.meta.env.VITE_API_URL}/api`, 
   headers: {
     'Content-Type': 'application/json',
   },
@@ -16,9 +16,7 @@ api.interceptors.request.use(
     }
     return config;
   },
-  (error) => {
-    return Promise.reject(error);
-  }
+  (error) => Promise.reject(error)
 );
 
 export default api;
